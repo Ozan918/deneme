ecd4a1dd1ada18e898cf684d7b7eceb4db581f03
Ozan918 <senecen04@gmail.com>
2025-12-12 04:02:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Render çalışıyor!")
+from aiohttp import web
+
+async def handle(request):
+    return web.Response(text="Bot çalışıyor!")
+
+app = web.Application()
+app.add_routes([web.get('/', handle)])
+
+if __name__ == "__main__":
+    web.run_app(app, host="0.0.0.0", port=10000)
